028e058feb0ef595824890c3af2ee7aebe5362ae
Deadly Artist <ocram.siebenstein@web.de>
2025-10-25 19:15:35 +0200

    works

--- 1.19.2/forge/src/main/java/keeptrident/forge/KPTridentForge.java
+++ 1.19.2/forge/src/main/java/keeptrident/forge/KPTridentForge.java
@@ -1,6 +1,5 @@
 package keeptrident.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -9,9 +8,6 @@ import keeptrident.KPTrident;
 @Mod(KPTrident.MOD_ID)
 public final class KPTridentForge {
     public KPTridentForge() {
-        // Submit our event bus to let Architectury API register our content on the right time.
-        EventBuses.registerModEventBus(KPTrident.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-
         // Run our common setup.
         KPTrident.init();
     }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # KeepTrident
 Minecraft mod to make loyalty tridents stay in the inventory when thrown. Say no to loot moving into your trident slot before your trident comes back! This also makes your trident completely unlosable.
+
+## Versions
+Loaders: Fabric, Forge, Neoforge
+Minecraft: 1.18.2 to 1.21.9
+
+## License
+This mod is licensed under MIT license.
+
+## Author
+
+[Deadly Artist](https://deadlyartist.com)
+
+See my website for my other projects: https://deadlyartist.com
